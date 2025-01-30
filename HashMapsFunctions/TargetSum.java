6ea0dc4e66e00231a92b7bf0b12b8c55752a8a42
TikeshKardate <tikesh.kardate@gmail.com>
2025-01-30 23:05:54 +0530

    DSA Day02 HashMaps&Functions programs added

---
+++ HashMapsFunctions/TargetSum.java
@@ -0,0 +1,41 @@
+/*
+Check for a Pair with Given Sum in an Array
+Problem: Given an array and a target sum, find if there exists a pair of elements whose sum is equal to the target.
+Hint: Store visited numbers in a hash map and check if target - current_number exists in the map.
+*/
+
+import java.util.*;
+import java.util.HashMap;
+public class TargetSum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+          System.out.print("Enter the size of the array: ");
+          int n=sc.nextInt();
+          System.out.println("Enter array elements : ");
+          int arr[]=new int[n];
+
+          for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+          }
+
+          System.out.print("Enter target sum: ");
+          int target=sc.nextInt();
+          boolean flag=false;
+
+          HashMap<Integer,Integer>mp=new HashMap<>();
+          int ans=0;
+          int i;
+
+          for( i=0;i<n;i++){
+            if(mp.containsKey(target - arr[i])){
+                  flag=true;
+                  break;
+                }
+
+            mp.put(arr[i],i);
+          }
+          if(flag) System.out.println("Pair is present ");
+          else System.out.println("Pair is not present ");
+    }
+}
---
+++ HashMapsFunctions/TargetSum2.java
@@ -0,0 +1,43 @@
+/*
+Two Sum Problem
+Problem: Given an array and a target sum, find two indices such that their values add up to the target.
+Hint: Use a hash map to store the index of each element as you iterate. Check if target - current_element exists in the map.
+*/
+
+import java.util.*;
+import java.util.HashMap;
+
+public class TargetSum2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+          System.out.print("Enter the size of the array: ");
+          int n=sc.nextInt();
+          System.out.println("Enter array elements : ");
+          int arr[]=new int[n];
+
+          for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+          }
+          
+          System.out.print("Enter target sum: ");
+          int target=sc.nextInt();
+          boolean flag=false;
+
+          HashMap<Integer,Integer>mp=new HashMap<>();
+          int ans=0;
+          int i;
+          for( i=0;i<n;i++){
+            if(mp.containsKey(target - arr[i])){
+                  flag=true;
+                  break;
+                }
+
+            mp.put(arr[i],i);
+          }
+
+          if(flag) System.out.println("Pair is present, indexes are "+ mp.get(target-arr[i]) + " and "+i );
+          else System.out.println("Pair is not present ");
+
+    }
+  }
